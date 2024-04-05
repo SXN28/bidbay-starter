a94312d54f24e6b951da297ad4384a4be480f767
p2200744 <clement.foray@etu.univ-lyon1.fr>
2024-04-05 17:30:34 +0200

    add report + mochawesome

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -6,8 +6,20 @@ import { getDetails } from '../validators/index.js'
 const router = express.Router()
 
 router.get('/api/products', async (req, res, next) => {
-  res.status(600).send()
-})
+  try {
+    const products = await Product.findAll({
+      include: {
+        model: User,
+        as: 'seller'
+      }
+    });
+    res.json(products);
+  } catch (error) {
+    // Gérer les erreurs de manière appropriée
+    console.error('Une erreur s\'est produite lors de la récupération des produits :', error);
+    res.status(500).json({ message: 'Une erreur s\'est produite lors de la récupération des produits' });
+  }
+});
 
 router.get('/api/products/:productId', async (req, res) => {
   res.status(600).send()
@@ -15,16 +27,51 @@ router.get('/api/products/:productId', async (req, res) => {
 
 // You can use the authMiddleware with req.user.id to authenticate your endpoint ;)
 
-router.post('/api/products', (req, res) => {
-  res.status(600).send()
-})
+router.post('/api/products', async (req, res) => {
+  try {
+    const { name, description, category, originalPrice, pictureUrl, endDate, sellerId } = req.body;
+
+    if (!name || !description || !category || !originalPrice || !endDate || !sellerId) {
+      return res.status(400).json({ message: 'Toutes les données nécessaires ne sont pas fournies' });
+    }
+
+    // Optionnel : Vérifier si l'utilisateur (sellerId) existe dans la base de données avant de créer le produit
+
+    const newProduct = await Product.create({
+      name,
+      description,
+      category,
+      originalPrice,
+      pictureUrl,
+      endDate,
+      sellerId,
+    });
+
+    res.status(201).json(newProduct);
+  } catch (error) {
+    // Gérer l'erreur correctement en renvoyant une réponse d'erreur au client
+    console.error('Une erreur s\'est produite lors de la création du produit :', error);
+    res.status(500).json({ message: 'Une erreur s\'est produite lors de la création du produit' });
+  }
+});
 
 router.put('/api/products/:productId', async (req, res) => {
   res.status(600).send()
 })
 
-router.delete('/api/products/:productId', async (req, res) => {
-  res.status(600).send()
-})
+router.delete('/api/products/:productId', authMiddleware, async (req, res, next) => {
+  try {
+    const productId = req.params.productId;
+    const product = await Product.findByPk(productId);
+    if (!product) {
+      return res.status(404).json({ message: 'Produit non trouvé' });
+    }
+    await product.destroy();
+
+    res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default router
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,13 +3,53 @@ import { ref, computed } from "vue";
 
 const loading = ref(false);
 const error = ref(false);
+const products = ref([]);
+
+function sortByName() {
+  products.value.sort((a, b) => a.name.localeCompare(b.name));
+}
+
+function sortByPrice() {
+  products.value.sort((a, b) => a.originalPrice - b.originalPrice);
+}
+
+
+let searchTerm = '';
+
+function filterProducts() {
+  searchTerm = searchTerm.toLowerCase().trim();
+  filteredProducts.value = searchTerm
+    ? products.value.filter(product => product.name.toLowerCase().includes(searchTerm))
+    : products.value.slice();
+  sortProducts();
+}
+
+function sortProducts() {
+  if (searchTerm) {
+    if (sortOption === 'name') {
+      sortByName();
+    } else {
+      sortByPrice();
+    }
+  }
+}
+
+const filteredProducts = computed(() => {
+  if (!searchTerm) {
+    return products.value.slice();
+  } else {
+    return products.value.filter(product => product.name.toLowerCase().includes(searchTerm));
+  }
+});
+
 
 async function fetchProducts() {
-  loading.value = true;
-  error.value = false;
-
   try {
-  } catch (e) {
+    const res = await fetch('http://localhost:3000/api/products/');
+    const data = await res.json();
+    products.value = data;
+  } catch (error) {
+    console.error('Une erreur s\'est produite lors de la récupération des produits :', error);
     error.value = true;
   } finally {
     loading.value = false;
@@ -17,6 +57,13 @@ async function fetchProducts() {
 }
 
 fetchProducts();
+
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+}
+
+
 </script>
 
 <template>
@@ -25,90 +72,76 @@ fetchProducts();
 
     <div class="row mb-3">
       <div class="col-md-6">
-        <form>
-          <div class="input-group">
-            <span class="input-group-text">Filtrage</span>
-            <input
-              type="text"
-              class="form-control"
-              placeholder="Filtrer par nom"
-              data-test-filter
-            />
-          </div>
-        </form>
-      </div>
-      <div class="col-md-6 text-end">
-        <div class="btn-group">
-          <button
-            type="button"
-            class="btn btn-primary dropdown-toggle"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-            data-test-sorter
-          >
-            Trier par nom
-          </button>
-          <ul class="dropdown-menu dropdown-menu-end">
-            <li>
-              <a class="dropdown-item" href="#"> Nom </a>
-            </li>
-            <li>
-              <a class="dropdown-item" href="#" data-test-sorter-price>
-                Prix
-              </a>
-            </li>
-          </ul>
+      <form>
+        <div class="input-group">
+          <span class="input-group-text">Filtrage</span>
+          <input
+            v-model="searchTerm"
+            @input="filterProducts"
+            type="text"
+            class="form-control"
+            placeholder="Filtrer par nom"
+            data-test-filter
+          />
         </div>
+      </form>
+    </div>
+
+
+
+
+
+      <div class="col-md-6 text-end">
+      <div class="btn-group">
+        <button
+          type="button"
+          class="btn btn-primary dropdown-toggle"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+          data-test-sorter
+        >
+          {{ sortOption === 'name' ? 'Trier par nom' : 'Trier par prix' }}
+        </button>
+        <ul class="dropdown-menu dropdown-menu-end">
+          <!-- Appel des méthodes de tri lorsque les options sont sélectionnées -->
+          <li><a class="dropdown-item" href="#" @click="sortByName"> Nom </a></li>
+          <li><a class="dropdown-item" href="#" @click="sortByPrice"> Prix </a></li>
+        </ul>
       </div>
     </div>
 
-    <div class="text-center mt-4" data-test-loading>
-      <div class="spinner-border" role="status">
-        <span class="visually-hidden">Chargement...</span>
-      </div>
+
+
+
     </div>
 
-    <div class="alert alert-danger mt-4" role="alert" data-test-error>
-      Une erreur est survenue lors du chargement des produits.
-    </div>
     <div class="row">
-      <div class="col-md-4 mb-4" v-for="i in 10" data-test-product :key="i">
+      <div class="col-md-4 mb-4" v-for="product in filteredProducts" :key="product.id">
         <div class="card">
-          <RouterLink :to="{ name: 'Product', params: { productId: 'TODO' } }">
-            <img
-              src="https://picsum.photos/id/403/512/512"
-              data-test-product-picture
-              class="card-img-top"
-            />
+          <RouterLink :to="{ name: 'Product', params: { productId: product.id } }">
+            <img :src="product.pictureUrl" data-test-product-picture class="card-img-top" />
           </RouterLink>
           <div class="card-body">
             <h5 class="card-title">
-              <RouterLink
-                data-test-product-name
-                :to="{ name: 'Product', params: { productId: 'TODO' } }"
-              >
-                Machine à écrire
+              <RouterLink :to="{ name: 'Product', params: { productId: product.id } }">
+                {{ product.name }}
               </RouterLink>
             </h5>
-            <p class="card-text" data-test-product-description>
-              Machine à écrire vintage en parfait état de fonctionnement
-            </p>
+            <p class="card-text" data-test-product-description>{{ product.description }}</p>
             <p class="card-text">
               Vendeur :
-              <RouterLink
-                data-test-product-seller
-                :to="{ name: 'User', params: { userId: 'TODO' } }"
-              >
-                alice
+              <RouterLink :to="{ name: 'User', params: { userId: product.sellerId } }">
+                {{ product.sellerId }}
               </RouterLink>
             </p>
-            <p class="card-text" data-test-product-date>
-              En cours jusqu'au 05/04/2026
-            </p>
-            <p class="card-text" data-test-product-price>Prix actuel : 42 €</p>
+            <p class="card-text" data-test-product-date>En cours jusqu'au {{ formatDate(product.endDate) }}</p>
+            <p class="card-text" data-test-product-price>Prix actuel : {{ product.originalPrice }} €</p>
           </div>
         </div>
       </div>
     </div>
+
+    
+    
   </div>
 </template>
